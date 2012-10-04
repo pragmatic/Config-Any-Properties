f814eb2464978d9ac2740974b87b0b4362004646
Peter Hallam <pragmatic@cpan.org>
2012-10-04 13:03:25 +0800

    First commit of Config-Any-Properties

---
+++ lib/Config/Any/Properties.pm
@@ -0,0 +1,31 @@
+#  COPYRIGHT: © 2012 Peter Hallam
+
+package Config::Any::Properties;
+
+#   ABSTRACT: This module does something
+#    CREATED: Thu, 4 Oct 2012 05:03:25 UTC
+#     AUTHOR: Peter Hallam <pragmatic@cpan.org>
+
+use strict;
+use warnings;
+use v5.10;
+
+# VERSION
+
+
+
+1;
+
+__END__
+
+=for Pod::Coverage
+
+=head1 SYNOPSIS
+
+ use Config::Any::Properties;
+
+ my $properties = Config::Any::Properties->new;
+
+=head1 DESCRIPTION
+
+=cut
